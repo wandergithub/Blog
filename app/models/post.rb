8996c2088cf07f6a2550171d860e83c2aaafa50e
wandergithub <wandergonzalez0@hotmail.com>
2022-11-22 12:39:31 -0400

    Create Post class methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def Post.update_posts_counter(user)
+    number = Post.where(user: user).count
+    user.update(PostsCounter: number)
+  end
+
+  def Post.most_rescent_comments(post)
+    Comment.where(post: post).order(:created_at).limit(5)
+  end
 end
