5580440c128e2f6fba15c04f6c5b5c9a02aa5082
wandergithub <wandergonzalez0@hotmail.com>
2022-11-22 12:15:58 -0400

    Create most rescent posts for a user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  
+  def most_rescent_posts()
+    Post.where(user: self).order(:created_at).limit(3)
+  end
 end
