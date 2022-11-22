c9ee2a1baf2e7a0dfb7f2dc0cf071f1fb089ba6c
wandergithub <wandergonzalez0@hotmail.com>
2022-11-22 15:04:40 -0400

    Set users class to be named as author in assosiations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   def self.update_comments_counter(post)
     comments_count = Comment.where(post:).count
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def self.most_rescent_posts(user)
+  def self.most_rescent_posts(au)
     Post.where(user:).order(:created_at).limit(3)
   end
 end
