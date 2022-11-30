fe93cd3dcf8c49071c8425edd96a6b3f742085ab
wandergithub <wandergonzalez0@hotmail.com>
2022-11-30 15:49:41 -0400

    Solve linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-   @user = User.find(params[:id])
-   @most_rescent_posts = User.most_rescent_posts(@user)
+    @user = User.find(params[:id])
+    @most_rescent_posts = User.most_rescent_posts(@user)
   end
 end
