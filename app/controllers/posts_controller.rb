e6674aa3e762a126a6cefe7429cc1495ebc42175
wandergithub <wandergonzalez0@hotmail.com>
2022-11-30 13:17:51 -0400

    create post#show view to display a post from an user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
-  def show; end
+  def show
+    @post = Post.where(user_id: params[:user_id]).find(params[:id])
+  end
 end
