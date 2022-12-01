ac1215c11d463e6b5fc7f6fb6d77aeb26a6aded6
wandergithub <wandergonzalez0@hotmail.com>
2022-12-01 16:07:56 -0400

    add form to create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,35 @@ class PostsController < ApplicationController
   def show
     @post = Post.where(user_id: params[:user_id]).find(params[:id])
   end
+
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :text))
+    post.user = current_user()
+    post.CommentsCounter = 0
+    post.LikesCounter = 0
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if post.save
+          # success message
+          flash[:success] = "Post saved successfully"
+          # redirect to index
+          redirect_to ('/users/1/posts')
+        else
+          # error message
+          flash.now[:error] = "Error: Post could not be saved"
+          # render new
+          render :new, locals: { post: post }
+        end
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
+  get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
-
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts/create', to: 'posts#create', as: 'create_post'
   # Defines the root path route ("/")
   # root "articles#index"
 end
