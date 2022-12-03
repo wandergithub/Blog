04883a4062e638244cca6d9ca86d7213d8383a60
wandergithub <wandergonzalez0@hotmail.com>
2022-12-02 20:35:57 -0400

    Rubocop

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id]);
-    like  = Like.new(user: current_user, post:)
+    post = Post.find(params[:post_id])
+    like = Like.new(user: current_user, post:)
     respond_to do |format|
       format.html do
         if like.save
-          flash[:success] = "Post liked"
+          flash[:success] = 'Post liked'
           Like.update_likes_counter(post)
         else
           flash[:error] = "Couldn't like the post"
