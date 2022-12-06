c1059c104f1a4bb0cab5558cb6f992715d1afbb8
wandergithub <wandergonzalez0@hotmail.com>
2022-12-06 10:13:31 -0400

    finish User index page specs

--- spec/features/users/users_index_spec.rb
+++ spec/features/users/users_index_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
-describe 'show one users', type: :feature do
+describe 'User index', type: :feature do
   context 'visit /users' do
     before(:example) { visit users_path }
+    before(:each) do
+      @users = User.all
+    end
+    it 'I can see the username of all other users.' do
+      @users.each do |user|
+        expect(page).to have_content(user.name)
+      end
+    end
+    it 'I can see the profile picture for each user.' do
+      expect(page).to have_css('img', count: @users.length)
+    end
+    it 'I can see the number of posts each user has written.' do
+      @users.each do |user|
+        expect(page).to have_content("Number of posts: #{user.PostsCounter}")
+      end
+    end
+    it "When I click on a user, I am redirected to that user's show page." do
+      user = User.first
 
-    it 'I can see the username of all other users.'
-    it 'I can see the profile picture for each user.'
-    it 'I can see the number of posts each user has written.'
-    it "When I click on a user, I am redirected to that user's show page."
+      expect(page).to have_current_path(users_path)
+      click_on "#{user.name}"
+      expect(page).to have_current_path(user_path(id: user.id))
+    end
   end
 end
