14b27881a8fd68d462d191025efc99e939d0ddd0
wandergithub <wandergonzalez0@hotmail.com>
2022-12-06 14:27:04 -0400

    create Post#show page specs

--- spec/features/posts/posts_show_spec.rb
+++ spec/features/posts/posts_show_spec.rb
@@ -2,14 +2,35 @@ require 'rails_helper'
 
 describe 'show first post complete', type: :feature do
   context 'visit /users/user_id/posts/post_id' do
-    before(:example) { visit "/users/#{Post.first.author.id}/posts/#{Post.first.id}" }
+    let(:post) do
+      Post.first
+    end
+    before(:example) { visit "/users/#{post.user.id}/posts/#{post.id}" }
 
-    it "I can see the post's title."
-    it 'I can see who wrote the post.'
-    it 'I can see how many comments it has.'
-    it 'I can see how many likes it has.'
-    it 'I can see the post body.'
-    it 'I can see the username of each commentor.'
-    it 'I can see the comment each commentor left.'
+    it "I can see the post's title." do
+      expect(page).to have_content(post.title)
+    end
+    it 'I can see who wrote the post.' do
+      expect(page).to have_content(post.user.name)
+    end
+    it 'I can see how many comments it has.' do
+      expect(page).to have_content("Comments: #{post.CommentsCounter}")
+    end
+    it 'I can see how many likes it has.' do
+      expect(page).to have_content("Likes: #{post.LikesCounter}")
+    end
+    it 'I can see the post body.' do
+      expect(page).to have_content(post.text.slice(0, 50))
+    end
+    it 'I can see the username of each commentor.' do
+      post.comments.each do |comment|
+        expect(page).to have_content(comment.user.name)
+      end
+    end
+    it 'I can see the comment each commentor left.' do
+      post.comments.each do |comment|
+        expect(page).to have_content(comment.text)
+      end
+    end
   end
 end
