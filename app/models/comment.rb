723f19730b0ea886b362ac43171057204f5274e7
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-08 16:04:03 -0400

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,14 +4,13 @@ class Comment < ApplicationRecord
 
   after_save :update_comments_counter
 
-	after_destroy :reduce_comments_counter
+  after_destroy :reduce_comments_counter
 
-	private
+  private
 
-	def reduce_comments_counter
-		post.decrement!(:CommentsCounter)
-	end
-
+  def reduce_comments_counter
+    post.decrement!(:CommentsCounter)
+  end
 
   def update_comments_counter
     post.increment!(:CommentsCounter)
