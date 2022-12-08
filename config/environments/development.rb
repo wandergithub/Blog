d38cc6cbb97cb6b07ef500c90aae9a16b8845529
wandergithub <wandergonzalez0@hotmail.com>
2022-12-08 02:57:41 -0400

    MIT licence

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   # config.action_mailer.delivery_method = :smtp
